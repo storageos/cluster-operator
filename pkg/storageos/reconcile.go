b49f6719e468deb9c199a935e3a963bab840c45d
Sunny <me@darkowlzz.space>
2018-08-17 05:28:12 +0530

    Check finalizers at reconcile
    Finalizers are set when the object should be deleted. Finalizers in a
    CRD metadata could be used as a hint for the controller to delete the
    dependent objects. Once all the controller actions are completed, the
    finalizer should be emptied for k8s to garbage collect it. This fixes
    the issue of hang at uninstall by emptying the finalizers in the
    controller.

--- pkg/storageos/reconcile.go
+++ pkg/storageos/reconcile.go
@@ -1,12 +1,26 @@
 package storageos
 
 import (
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+
 	api "github.com/storageos/storageos-operator/pkg/apis/node/v1alpha1"
 )
 
 func Reconcile(m *api.StorageOS) error {
-	if err := deployStorageOS(m); err != nil {
-		return err
+	// Finalizers are set when an object should be deleted. Apply deploy only
+	// when finalizers is empty.
+	if len(m.GetFinalizers()) == 0 {
+		if err := deployStorageOS(m); err != nil {
+			return err
+		}
+	} else {
+		// Reset finalizers and let k8s delete the object.
+		// When finalizers are set on an object, metadata.deletionTimestamp is
+		// also set. deletionTimestamp helps the garbage collector identify
+		// when to delete an object. k8s deletes the object only once the
+		// list of finalizers is empty.
+		m.SetFinalizers([]string{})
+		return sdk.Update(m)
 	}
 
 	return nil
