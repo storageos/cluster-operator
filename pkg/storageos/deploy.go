04cb19df889963f2998f8ebcb26b4b634a7bc1f9
Sunny <me@darkowlzz.space>
2019-03-17 19:38:35 +0530

    cluster: fix cluster status update
    Combine current cluster and deployment in to a single data type
    StorageOSCluster so that the deployment always points to the right
    cluster.
    
    Also, adds status members checks in e2e test and enables go race detector
    for the unit tests.

--- pkg/storageos/deploy.go
+++ pkg/storageos/deploy.go
@@ -18,9 +18,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/client-go/tools/record"
-
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -109,30 +106,6 @@ const (
 	defaultUsername = "storageos"
 	defaultPassword = "storageos"
 )
-
-// Deployment stores all the resource configuration and performs
-// resource creation and update.
-type Deployment struct {
-	client     client.Client
-	stos       *api.StorageOSCluster
-	recorder   record.EventRecorder
-	k8sVersion string
-	scheme     *runtime.Scheme
-	update     bool
-}
-
-// NewDeployment creates a new Deployment given a k8c client, storageos manifest
-// and an event broadcast recorder.
-func NewDeployment(client client.Client, stos *api.StorageOSCluster, recorder record.EventRecorder, scheme *runtime.Scheme, version string, update bool) *Deployment {
-	return &Deployment{
-		client:     client,
-		stos:       stos,
-		recorder:   recorder,
-		k8sVersion: version,
-		scheme:     scheme,
-		update:     update,
-	}
-}
 
 // Deploy deploys storageos by creating all the resources needed to run storageos.
 func (s *Deployment) Deploy() error {
