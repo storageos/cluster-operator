be08b05b22ee71b0612e1dc30db4b5dd3f811da7
Sunny <me@darkowlzz.space>
2019-04-21 16:43:22 +0530

    Trigger rhel container service build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,8 @@ if [ "$1" = "tagged" ]; then
     docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
     docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${TRAVIS_TAG}"
     docker push "${IMAGE_NAME}:latest" && docker push "${IMAGE_NAME}:${TRAVIS_TAG}"
+    # Trigger a rhel container service build.
+    curl -X POST -k -H 'Content-Type: application/json' -i https://connect.redhat.com/api/v2/projects/$RH_PID/build --data '{"tag": "'"${IMAGE_TAG}"'"}'
 elif [ "$1" = "develop" ]; then
     docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:develop"
     docker push "${IMAGE_NAME}:develop"
