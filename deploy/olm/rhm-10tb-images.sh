5c377b8ad2dea6ccc92af6ca428ae25734126b2e
Simon Croome <simon.croome@storageos.com>
2020-02-07 14:06:40 +0000

    check for changes file before comparing

--- deploy/olm/rhm-10tb-images.sh
+++ deploy/olm/rhm-10tb-images.sh
@@ -1,1 +1,2 @@
+# Overrides images set in default-images.sh
 RELATED_IMAGE_STORAGEOS_NODE=registry.connect.redhat.com/storageos/node-10tb:${NEW_VERSION}
--- deploy/olm/rhm-1tb-images.sh
+++ deploy/olm/rhm-1tb-images.sh
@@ -1,1 +1,2 @@
+# Overrides images set in default-images.sh
 RELATED_IMAGE_STORAGEOS_NODE=registry.connect.redhat.com/storageos/node-1tb:${NEW_VERSION}
