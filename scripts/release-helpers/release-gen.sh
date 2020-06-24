535794ae16cdf572ff912d9cb76f02b11164889f
Sunny <darkowlzz@protonmail.com>
2020-06-24 16:58:33 +0530

    release-gen: remove v prefix from csv replaces
    `NEW_VERSION` in release-gen expects the version to have a "v" prefix
    now. Remove the "v" prefix from replaces field in CSV.

--- scripts/release-helpers/release-gen.sh
+++ scripts/release-helpers/release-gen.sh
@@ -77,7 +77,7 @@ spec.install.spec.deployments[0].spec.template.spec.containers[0].env[8].value: 
 spec.install.spec.deployments[0].spec.template.spec.containers[0].env[9].value: ""
 spec.install.spec.deployments[0].spec.template.spec.containers[0].env[10].value: ""
 spec.install.spec.deployments[0].spec.template.spec.containers[0].env[11].value: ""
-spec.replaces: storageosoperator.v$PREV_VERSION
+spec.replaces: storageosoperator.$PREV_VERSION
 EOF
 echo
 
@@ -171,7 +171,7 @@ spec.install.spec.deployments[0].spec.template.spec.containers[0].env[9].value: 
 spec.install.spec.deployments[0].spec.template.spec.containers[0].env[10].value: ""
 spec.install.spec.deployments[0].spec.template.spec.containers[0].env[11].value: ""
 spec.customresourcedefinitions.owned[2].specDescriptors[0].description: The StorageOS Node image to upgrade to. e.g. \`registry.connect.redhat.com/storageos/node:latest\`
-spec.replaces: storageosoperator.v$PREV_VERSION
+spec.replaces: storageosoperator.$PREV_VERSION
 EOF
 echo
 
